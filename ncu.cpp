7261cfe468ce4e398e1c780677c7708a0b1f5724
thisguyistotallyben <thisguyistotallyben@gmail.com>
2017-12-02 18:31:39 -0500

    showing elements is broken

--- ncu.cpp
+++ ncu.cpp
@@ -6,9 +6,11 @@
 
 
 #include <iostream>
+#include <cstdio>
 #include <vector>
 #include <map>
 #include <ncurses.h>
+#include <panel.h>
 #include "ncu.h"
 
 using namespace std;
@@ -24,14 +26,59 @@ void NCU::start() {
     init_pair(1, COLOR_BLACK, COLOR_WHITE);
     noecho();
     refresh();
+    NCU_STARTED = true;
+    r = LINES;
+    c = COLS;
 }
 
-void NCU::addElement(string name, elementType type,
-                int sizex, int sizey,
-                int posx, int posy) {
+void NCU::addElement(string id, elementType type, int sizex, int sizey, int posx, int posy) {
+    check_if_started();
 
+    Element *e;
+    
+    e = new Element;
+
+    // fill in info
+    e->etype = type;
+
+    // add to map
+    elementList.insert(make_pair(id, e));
+}
+
+void NCU::write(string id, string data, int posx, int posy) {
+    WINDOW *win = getWin(id);
+    if (win != NULL) mvwprintw(win, posx, posy, data.c_str());
+}
+
+void showElement(string id) {
+    show_win(getWin(id));
 }
 
 void NCU::end() {
     endwin();
+}
+
+void NCU::check_if_started() {
+    if (!NCU_STARTED) {
+        cout << "NCU NOT INTIALIZED. YOU MUST CALL start()\n";
+        exit(1);
+    }
+}
+
+// utilitarian crap -----------------------------------------------
+
+int NCU::cols() {
+    return c;
+}
+
+int NCU::rows() {
+    return r;
+}
+
+WINDOW* NCU::getWin(string id) {
+    map<string, Element*>::iterator e;
+
+    e = elementList.find(id);
+    if (e != elementList.end()) return e->second->win;
+    else return NULL;
 }
--- ncu.h
+++ ncu.h
@@ -10,20 +10,27 @@
 
 #include <string>
 #include <vector>
+#include <ncurses.h>
+#include <panel.h>
 #include <map>
 
 using namespace std;
 
 
 enum elementType {
-    NCU_BASIC_BOX,
+    NCU_BORDER_BOX,
+    NCU_BORDERLESS_BOX,
     NCU_BASIC_INPUT
 };
 
 class Element {
     public:
-    private:
-        string name;
+        string id;
+        elementType etype;
+        WINDOW *win;
+        PANEL  *panel;
+
+        bool visible;
 };
 
 class NCU {
@@ -31,11 +38,22 @@ class NCU {
         void start();
         void end();
 
-        void addElement(string name, elementType type,
+        void addElement(string id, elementType type,
                         int sizex, int sizey,
                         int posx, int posy);
+        void showElement(string id);
+        void write(string id, string data, int posx, int posy);
+        int cols();
+        int rows();
     private:
+        void check_if_started();
+        WINDOW* getWin(string id);
+
         map<string, Element*> elementList;
+        bool NCU_STARTED = false;
+        string focus;
+        int r = 0;
+        int c = 0;
 };
 
 #endif
