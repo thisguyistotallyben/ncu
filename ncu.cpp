ddf723e6621ac4e1a614a9cfa2c5bbee6d6bf82f
thisguyistotallyben <thisguyistotallyben@gmail.com>
2017-12-03 16:03:50 -0500

    does some more things now

--- ncu.cpp
+++ ncu.cpp
@@ -11,12 +11,13 @@
 #include <map>
 #include <ncurses.h>
 #include <panel.h>
+#include <cstdarg>
 #include "ncu.h"
 
 using namespace std;
 
 
-// NCU -----------------------------------------------------------------
+// setup and cleanup -----------------------------------------------------------------
 
 void NCU::start() {
     initscr();
@@ -35,42 +36,173 @@ void NCU::hideCursor() {
     cursor = false;
     curs_set(0);
 }
-void NCU::addElement(string id, elementType type, int sizex, int sizey, int posx, int posy) {
+
+void NCU::end() {
+    map<string, Element*>::iterator eit;
+    
+    // clean up elements
+    for (eit = elementList.begin(); eit != elementList.end(); eit++) {
+        delete eit->second;
+    }
+
+    endwin();
+}
+
+// element creation and manipulation -------------------------------------------------
+
+// addElement
+// creates a new element and adds it to the element list
+void NCU::addElement(string id, borderType bt, int sizex, int sizey, int posx, int posy) {
+    Element *e;
+
+    // error checking
     check_if_started();
 
-    Element *e;
-    
+    // setup
     e = new Element;
 
     // fill in info
-    e->etype = type;
+    e->id = id;
+    e->title = "";
+    e->btype = bt;
     e->win = newwin(sizey, sizex, posy, posx);
-
-    if (type == NCU_BORDER_BOX) box(e->win, 0, 0);
-    if (type == NCU_BASIC_INPUT) wborder(getWin(id), ' ', ' ', ' ', 0, ' ', ' ', 0, 0);
 
     // add to map
     elementList.insert(make_pair(id, e));
+
+    // make the proper borders
+    switch(e->btype) {
+        case NCU_BORDERLESS_BOX:
+            wborder(e->win, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+            break;
+        case NCU_BORDER_BOX:
+            box(e->win, 0, 0);
+            break;
+        case NCU_BASIC_INPUT:
+            wborder(e->win, ' ', ' ', ' ', 0, ' ', ' ', 0, 0);
+            break;
+        default: break;
+    }
 }
 
+// borderElement
+// puts a border on an element after creation
+// NOTE: shows element even if not visible
+void NCU::borderElement(string id, borderType bt) {
+    Element *e = getElement(id);
+
+    if (e != NULL) {
+        if (bt != NCU_NO_CHANGE) e->btype = bt;
+
+        switch(e->btype) {
+            case NCU_BORDERLESS_BOX:
+                wborder(e->win, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+                break;
+            case NCU_BORDER_BOX:
+                box(e->win, 0, 0);
+                break;
+            case NCU_BASIC_INPUT:
+                wborder(e->win, ' ', ' ', ' ', 0, ' ', ' ', 0, 0);
+                break;
+            default: break;
+        }
+    }
+
+    // add title back if neccessary
+    if (e->title != "") addTitle(id, e->title);
+
+    // refresh
+    wrefresh(e->win);
+}
+
+// updateElement
+// pretty much the same thing as showElement
+// probably useless, idk
+void NCU::updateElement(string id) {
+    wrefresh(getWin(id));
+}
+
+// addTitle
+// adds a title to an element
+// TODO: maybe add possible positons
 void NCU::addTitle(string id, string title) {
-    title = " " + title + " ";
-    this->write(id, title, 1, 0);
+    (getElement(id))->title = title;
+
+    if (title != "") {
+        title = " " + title + " ";
+        this->write(id, title, 1, 0);
+    }
+    else borderElement(id, NCU_NO_CHANGE);
 }
 
+// clearElement
+// clears all the data in an element except the title
+void NCU::clearElement(string id) {
+    WINDOW *win;
+    win = getWin(id);
+
+    werase(win);
+    borderElement(id, NCU_NO_CHANGE);
+}
+
+// write
+// writes to an element
+// not to be confused with the real write
+// that'll probably come back and bite me in the butt
 void NCU::write(string id, string data, int posx, int posy) {
     WINDOW *win = getWin(id);
     if (win != NULL) mvwprintw(win, posy, posx, data.c_str());
 }
 
+// read
+// reads from an element
+// not to be confused with the real read
+// also will probably bite me in the butt later
+// TODO: add to only read from valid input boxes
+string NCU::read(string id) {
+    char c;
+    string s;
+    WINDOW *win;
+
+    // setup
+    win = getWin(id);
+    cbreak();
+    if(!cursor) curs_set(1);
+    wmove(win, 1, 2);
+    
+    while (1) {
+        c = wgetch(win);
+        switch(c) {
+            case 10: // enter
+                //clearWin(name);
+                curs_set(0);
+                return s;
+            default:
+                s = s + c;
+        }
+
+        
+    }
+    
+    if (!cursor) curs_set(0);
+}
+
+// showElement
+// initially shows an element
 void NCU::showElement(string id) {
     wrefresh(getWin(id));
 }
 
-void NCU::end() {
-    endwin();
+// wait
+// just a while loop waiting for a key
+// i'm not completely sure y tho
+void NCU::wait(char key) {
+    char c;
+    while (c = getch() != key);
 }
 
+// check_if_started()
+// checks if ncu has been started
 void NCU::check_if_started() {
     if (!NCU_STARTED) {
         cout << "NCU NOT INTIALIZED. YOU MUST CALL start()\n";
@@ -93,5 +225,12 @@ WINDOW* NCU::getWin(string id) {
 
     e = elementList.find(id);
     if (e != elementList.end()) return e->second->win;
+    else return NULL;
+}
+
+Element* NCU::getElement(string id) {
+    map<string, Element*>::iterator e;
+    e = elementList.find(id);
+    if (e != elementList.end()) return e->second;
     else return NULL;
 }
--- ncu.h
+++ ncu.h
@@ -13,20 +13,23 @@
 #include <ncurses.h>
 #include <panel.h>
 #include <map>
+#include <cstdarg>
 
 using namespace std;
 
 
-enum elementType {
+enum borderType {
     NCU_BORDER_BOX,
     NCU_BORDERLESS_BOX,
-    NCU_BASIC_INPUT
+    NCU_BASIC_INPUT,
+    NCU_NO_CHANGE // users: don't use this
 };
 
 class Element {
     public:
         string id;
-        elementType etype;
+        string title;
+        borderType btype;
         WINDOW *win;
         PANEL  *panel;
 
@@ -38,20 +41,29 @@ class NCU {
         void start();
         void end();
 
-        void addElement(string id, elementType type,
+        void addElement(string id, borderType bt,
                         int sizex, int sizey,
                         int posx, int posy);
+        void addGroup(string id, ...);
+        void borderElement(string id, borderType bt);
         void addTitle(string id, string title);
         void showElement(string id);
+        void clearElement(string id);
         void write(string id, string data, int posx, int posy);
+        string read(string id);
+        void updateElement(string id);
 
         void hideCursor();
+
+        void wait(char key);
 
         int width();
         int height();
     private:
         void check_if_started();
         WINDOW* getWin(string id);
+        Element* getElement(string id);
+        void borderElement(string id);
 
         map<string, Element*> elementList;
         bool NCU_STARTED = false;
